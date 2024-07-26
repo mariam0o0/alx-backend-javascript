f0a2d8d2cd5f4e861899c24fca6c03db9faf9106
Mariam <mariam.yasser772000@gmail.com>
2024-07-26 19:44:54 +0000

    0x02

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,47 +1,32 @@
-
-/* eslint-disable no-underscore-dangle */
 export default class Currency {
-    /**
-     * @param {string} code - currency code
-     * @param {string} name - currency name
-     *
-     * Implement getters and setters for each
-     */
-    constructor(code, name) {
-      this.code = code;
-      this.name = name;
-    }
-  
-    get code() {
-      return this._code;
-    }
-  
-  /**
-   * @param {String} code
-   */
-    set code(value) {
-      if (typeof value !== 'string') {
-        throw new Error('code must be a string');
-      }
-      this._code = value;
-    }
-  
-    get name() {
-      return this._name;
-    }
-
-  /**
-   * @param {String} name
-   */    
-    set name(value) {
-      if (typeof value !== 'string') {
-        throw new Error('name must be a string');
-      }
-      this._name = value;
-    }
-  
-    displayFullCurrency() {
-      return `${this.name} (${this.code})`;
-    }
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
   }
-  
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    if ((typeof code !== 'string') && !(code instanceof String)) {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if ((typeof name !== 'string') && !(name instanceof String)) {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+}
