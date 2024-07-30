3b20e42f90cd277d63cbd0095cb20c2f8e514e31
Mariam <mariam.yasser772000@gmail.com>
2024-07-30 23:09:12 +0000

    0x03

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,9 @@
+const getListStudentIds = (listStudents) => {
+  let listStudentIds = [];
+  if (!(listStudents instanceof Array)) {
+    return listStudentIds;
+  }
+  listStudentIds = listStudents.map((student) => student.id);
+  return listStudentIds;
+};
+export default getListStudentIds;
